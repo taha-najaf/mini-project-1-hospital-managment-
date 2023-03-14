bd3f9fca5db6025fbf40bb1e63d92c5cd86ee338
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-03-14 22:30:51 +0330

    design console

--- Program.cs
+++ Program.cs
@@ -24,8 +24,10 @@ namespace mini_project_1_hospital_managment_
             //============================ base menu
             while (true)
             {
+                Console.BackgroundColor = ConsoleColor.DarkCyan;
                 Console.WriteLine("choose one :");
-                Console.WriteLine("Admin(a)\nDoctor(d)\nNurse(n)");
+                Console.BackgroundColor= ConsoleColor.Black;
+                Console.WriteLine("Admin(a)\nDoctor(d)\nNurse(n)\nExit(e)");
                 //===============================  admin menu
                 string choose = Console.ReadLine();
                 if (choose == "a")
@@ -36,17 +38,39 @@ namespace mini_project_1_hospital_managment_
                     Console.WriteLine("Enter Password");
                     string password = Console.ReadLine();
                     if (username == admin.username && password == admin.password)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Green;
                         Console.WriteLine("correct!");
+                        Console.BackgroundColor= ConsoleColor.Black;
+                    }
                     else
                     {
+                        Console.BackgroundColor = ConsoleColor.Red;
                         Console.WriteLine("username or password is wrong");
+                        Console.BackgroundColor = ConsoleColor.Black;
                         continue;
                     }
                     ///========= abilities of admin
                     Console.WriteLine("1.Add a patient(a)\n2.List of patients(p)\n3.Add a doctor(b)\n4.List of doctors(d)\n5.Add a nurse(c)\n6.List of nurses(n)\n7.Exit(e)");
                     choose = Console.ReadLine();
+                    // list of patient
+                    if (choose == "p")
+                    {
+                        if (p == 0)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.WriteLine("There is not any patient in the hospital!");
+                        }
+                        for (int i = 0; i < p; i++)
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkCyan;
+                            Console.WriteLine("Information of patient {0}: ", i + 1);
+                            patients[i].information_patient();
+
+                        }
+                    }
                     //========= add patient
-                    if (choose == "a")
+                    else if (choose == "a")
                     {
                         patients[p] = new Patient();
                         Console.WriteLine("First Name: "); patients[p].firstname = Console.ReadLine();
@@ -56,62 +80,62 @@ namespace mini_project_1_hospital_managment_
                         Console.WriteLine("Doctor: "); patients[p].doctor = Console.ReadLine();
                         Console.WriteLine("Room Number"); patients[p].Room_number = int.Parse(Console.ReadLine());
                         p++;
+                        Console.BackgroundColor= ConsoleColor.Green;
                         Console.WriteLine("Patient added successfully!");
-                    } // list of patient
-                    else if (choose == "p")
-                    {
-                        for (int i = 0; i < n; i++)
-                        {
-                            Console.WriteLine("patient {0} informations are: ", i + 1);
-                            patients[i].information_patient();
-
-                        }
+                        Console.BackgroundColor = ConsoleColor.Black;
                     }
                     // add doctor
-                    else if(choose=="b")
+                    else if (choose == "b")
                     {
                         Console.WriteLine("Enter firstname: "); string firstname = Console.ReadLine();
-                        Console.WriteLine("Last Name: ");       string lastname = Console.ReadLine();
-                        Console.WriteLine("Id: ");              int id = int.Parse(Console.ReadLine());
-                        Console.WriteLine("Speciality: ");      string speciality = Console.ReadLine();
-                        Console.WriteLine("record: ");          int record = int.Parse(Console.ReadLine());
-                        doctors[d] = new Doctor(firstname,lastname,id,speciality,record);
+                        Console.WriteLine("Last Name: "); string lastname = Console.ReadLine();
+                        Console.WriteLine("Id: "); int id = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Speciality: "); string speciality = Console.ReadLine();
+                        Console.WriteLine("record: "); int record = int.Parse(Console.ReadLine());
+                        doctors[d] = new Doctor(firstname, lastname, id, speciality, record);
                         d++;
+                        Console.BackgroundColor = ConsoleColor.Green;
                         Console.WriteLine("Doctor added successfully!");
+                        Console.BackgroundColor = ConsoleColor.Black;
                     }//list of doctor 
-                    else if (choose == "d") 
+                    else if (choose == "d")
                     {
-                        for(int i=0;i<d;i++)
+                        for (int i = 0; i < d; i++)
                         {
-                            Console.WriteLine("doctor {0} informations are: ", i+1);
+                            Console.BackgroundColor = ConsoleColor.DarkBlue;
+                            Console.WriteLine("Information of doctor {0}: ", i + 1);
                             doctors[i].information_doctors();
 
                         }
                     }
                     //add nurse
-                    else if (choose=="c")
+                    else if (choose == "c")
                     {
                         Console.WriteLine("Enter firstname: "); string firstname = Console.ReadLine();
                         Console.WriteLine("Last Name: "); string lastname = Console.ReadLine();
                         Console.WriteLine("Id: "); int id = int.Parse(Console.ReadLine());
-                        nurses[n]=new Nurse(firstname,lastname,id);
+                        nurses[n] = new Nurse(firstname, lastname, id);
                         n++;
+                        Console.BackgroundColor = ConsoleColor.Green;
                         Console.WriteLine("Nurse added successfully!");
+                        Console.BackgroundColor = ConsoleColor.Black;
                     }// list of nurse 
-                    else if (choose == "n") 
+                    else if (choose == "n")
                     {
                         for (int i = 0; i < n; i++)
                         {
-                            Console.WriteLine("nurse {0} informations are: ", i + 1);
+                            Console.BackgroundColor = ConsoleColor.DarkYellow;
+                            Console.WriteLine("Information of nurse {0} : ", i + 1);
                             nurses[i].information_nurses();
 
                         }
                     }
-                }
                     else if (choose == "e") { break; }
+                }
 
-                //===== end admin
-                //=====Doctor menu
+
+                //==================================================================== end admin
+                //Doctor menu
                 else if (choose == "d")
                 {
                     Console.WriteLine("1.Visit patient(v)\n2.Prescribe(p)\n3.Exit(e)");
@@ -121,32 +145,41 @@ namespace mini_project_1_hospital_managment_
                         Console.WriteLine("Enter Name or Id: ");
                         string name = Console.ReadLine();
                         Console.WriteLine("Write drugs: something");
+                        Console.BackgroundColor = ConsoleColor.Green;
                         Console.WriteLine("prescribe successfully!");
+                        Console.BackgroundColor = ConsoleColor.Black;
                     }
-                    else if(choose == "v")
+                    else if (choose == "v")
                     {
+                        Console.BackgroundColor = ConsoleColor.Green;
                         Console.WriteLine("visit successfully!");
+                        Console.BackgroundColor = ConsoleColor.Black;
                     }
                     else if (choose == "e") { break; }
                 }
                 //====== end doctor menu
                 //======Nurse menu
-                else if(choose=="n")
+                else if (choose == "n")
                 {
                     Console.WriteLine("1.Treat patient(t)\n2.Exit(e)");
-                    choose= Console.ReadLine();
-                    if(choose=="t")
+                    choose = Console.ReadLine();
+                    if (choose == "t")
                     {
                         Console.WriteLine("Enter National_Code or name: ");
                         Console.ReadLine();
+                        Console.BackgroundColor = ConsoleColor.Green;
                         Console.WriteLine("patient treat successfully!");
+                        Console.BackgroundColor = ConsoleColor.Black;
                     }
                 }
+                else if (choose == "e")
+                    break;
 
 
             }
-
+            Console.BackgroundColor = ConsoleColor.Green;
             Console.WriteLine("Thank for using our system!!");
+            Console.ReadKey();
 
 
         }
