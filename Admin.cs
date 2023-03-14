de8db6763171982ff2664513f7bd49ed11a46b94
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-03-14 14:52:02 +0330

    admin class edit

--- Admin.cs
+++ Admin.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,23 +9,13 @@ namespace mini_project_1_hospital_managment_
 {
     internal class Admin
     {
-        public string username { get; set; }
-        private long password = 0;
-        public long Password
+        public string username ;
+        public string password ;
+        public Admin()
         {
-            get
-            {
-
-                return this.password;
-            }
-            set
-            { 
-                if (value<0)
-                    this.password = -1*(value);
-                else
-                    this.password = value;
-            }
+            this.password = "****";
+            this.username = "Admin";
         }
-        
+          
     }
 }
